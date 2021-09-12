7277fee9be06075ddaa06ca42b5c1ab928f2f3d7
Sparkho <serhiiparkho@gmail.com>
2021-09-12 08:52:56 +0300

    burger added

--- modest.js
+++ modest.js
@@ -22,3 +22,19 @@ modal.addEventListener("click", function (e) {
 
 new Splide( '.splide' ).mount();
 
+let mobileMenu = document.querySelector(".nav-mobile-menu");
+let mainMenu = document.querySelector(".navigation");
+
+mobileMenu.addEventListener("click", function() {
+    mobileMenu.classList.toggle("active-menu");
+    if(mobileMenu.classList.contains("active-menu")) {
+        mainMenu.classList.add("active-menu");
+    } else {
+        mainMenu.classList.remove("active-menu");
+    }
+});
+mainMenu.addEventListener("click", function(){
+    mainMenu.classList.toggle("active-menu");
+    mobileMenu.classList.toggle("active-menu");
+});
+
